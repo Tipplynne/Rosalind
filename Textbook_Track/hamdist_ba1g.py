b15472e690f20087ac716aad6dbbe5a0b010fde4
Yokobayashi Unit <yokobayashiunit@OIST-no-iMac-2.local>
2017-03-21 15:36:26 +0900

    A better written Hd calculator :)

---
+++ Textbook_Track/hamdist_ba1g.py
@@ -0,0 +1,14 @@
+#solution to problem BA1G in rosalind's textbook track set to calculate the hamming distance (no. of mismatches) between two sequences, p and q, usually denoted HammingDistance(p,q)
+
+#create function
+def HammingDistance(p,q):
+ print(sum(b1!=b2 for b1,b2 in zip(p,q)))
+
+#Get the sequences
+with open('rosalind_ba1g.txt', 'r') as inputfile:
+ p = inputfile.readline().strip()
+ q = inputfile.readline().strip()
+
+HammingDistance(p,q)
+
+#Note: can also use biopython's pairwise2.align.global function, which may be more applicable to later problems, but here is just overkill
