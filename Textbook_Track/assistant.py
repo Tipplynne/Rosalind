3a1448f831df066bf7978c02f4828bd00bf8be54
Yokobayashi Unit <yokobayashiunit@OIST-no-iMac-2.local>
2017-03-22 15:23:18 +0900

    hmmm. this one is going to be tricky in terms of time constraints

---
+++ Textbook_Track/assistant.py
@@ -0,0 +1,83 @@
+#This is code, taken from another coding project of mine, needs to be modified and used to create the library of all possible, single, double and triple replacement mutants for each kmer in the problem solution ba1i
+
+#pattern constraints k <= 12, d <= 3
+
+wt = ACGTTGCATGTC
+
+def single_mutants(wt, key):
+
+ #Read in the sequence and send to a dataframe
+ table = []
+ table.append(list(wt))
+ df = pd.DataFrame(table)
+ mut_colno = len(wt) 
+
+ #prepare a list of available bases
+ bases = ['A', 'C', 'G', 'T']
+ altbase = []
+
+ #set the mutant seq number counter
+ seq = 0
+ counter = 0
+
+ #iterate through the base list, until we hit a match with bases of the wildtype sequence (df columns)
+ for base in bases:
+  for pos in df.columns:
+
+   #create regext of current position, to search mutation info, preventing back-mutations)
+   pattern = r'{}'.format(pos+1)
+   matchObj = re.search(pattern, key)
+
+   #so if the base matches the wildtype and the position of previous mutation has not been covered
+   if base == df.ix[0,pos] and matchObj == None:
+    
+    #prepare a list of alternate bases and use those to start new corresponding mutant seqs
+    altbase.extend(bases)
+    altbase.remove(base)
+    df.set_value(seq+1, pos, altbase.pop())
+    df.set_value(seq+2, pos, altbase.pop())
+    df.set_value(seq+3, pos, altbase.pop())
+
+    #summarise mutation info to new column, provided the mutants have not been seen before
+    df.set_value(seq+1, mut_colno, '{}{}{}, {}'.format(base,pos+1,df.ix[seq+1,pos],key))
+    df.set_value(seq+2, mut_colno, '{}{}{}, {}'.format(base,pos+1,df.ix[seq+2,pos],key))
+    df.set_value(seq+3, mut_colno, '{}{}{}, {}'.format(base,pos+1,df.ix[seq+3,pos],key))
+    seq += 3
+ 
+ #replace the rest of the dataframe (empty mutant seq bases) with wildtype bases
+ df = df.fillna(df.ix[0,df.columns])
+ 
+ #change the column name for the mutations
+ df = df.rename(columns = {mut_colno: 'Mutation'})
+
+ #drop the wild type and rows where no mutation info was set, i.e where the same mutation has already happened
+ df = df[df.Mutation.notnull()]
+
+ return df.reset_index(drop = True)
+
+
+def library_maker(df):
+ #call the single mutants function and transpose the df, in order to count right (i.e., along the index)
+ dft = df.T
+ shape = len(df.columns) #to get column index, which is the mutation info, which will be used to create the keys
+ mutant_dictionary = {}
+ 
+ #put sequences in the library for the double replacement mutant library
+ for mu in dft:
+  mutant_dictionary.update({df.ix[mu,(shape-1)] : df.ix[mu,0:(shape-1)].str.cat()})
+ 
+ return mutant_dictionary
+
+
+def double_mutants(library):
+ 
+#essentially what we're doing here is looping through the single mutant library, calling the single_mutant function again on each single_mutant to make the double mutants (this will naturally create duplicates). serially append each sub df to make double mutant df, then drop duplicates.
+ ddf = pd.DataFrame()
+ shape = len(ddf.columns)
+
+ for key, seq in library.items():
+  ddf = ddf.append(single_mutants(seq, key), ignore_index = True)
+ 
+ return ddf.drop_duplicates(ddf.columns[0:(shape-1)], keep = 'last').reset_index(drop = True)
+
+                
---
+++ Textbook_Track/messy_freqword_ba1i.py
@@ -0,0 +1,55 @@
+#this is a solution to Project Rosalind's BA1I problem, which uses the HammingDistance function to find the most frequent words/patterns of length k (above a certain mismatch threshold, d) within a larger string of text
+
+
+#function to find and store k-mer positions in a list, mod from ba1d
+def messy_posfind(dna, pattern, k, d):
+ i = 0
+ posse = [pattern]
+ while i <= (len(dna) - k):
+  if (HammingDistance(dna[i:i+k], pattern)) <= d:
+   posse.append(dna[i:i+k])
+   i += 1
+  else:
+   i += 1
+ return posse
+
+#function to split the DNA into k-mers, 
+def dnaread(dna, k):
+ i = 0
+ kmers = []
+ while i <= (len(dna) - k):
+  kmers.append(dna[i:i+k])
+  i += 1
+ return set(kmers)
+
+#Hd function
+def HammingDistance(p,q):
+ return(sum(b1!=b2 for b1,b2 in zip(p,q)))
+
+#use a fxn to find multiple modes
+def mode(array):
+    most = max(list(map(array.count, array)))
+    return list(set(filter(lambda x: array.count(x) == most, array)))
+
+#Get the input data
+with open('rosalind_ba1i.txt', 'r') as inputfile:
+ dna = inputfile.readline().strip()
+ inps = inputfile.readline().strip().split(" ")
+ k, d = int(inps[0]), int(inps[1])
+
+#run fxns
+kmers = dnaread(dna, k)
+
+#open dictionary of matched mismatches
+occurance = []
+for kmer in kmers:
+ occurance.append(messy_posfind(dna, kmer, k, d))
+
+print(occurance)
+
+max_kmers = mode(occurance)
+
+for word in max_kmers:
+ print(word, end=' ')
+ 
+
