b50f89389b68377d9af6532f07888dde056f68d7
Yokobayashi Unit <yokobayashiunit@OIST-no-iMac-2.local>
2017-04-01 16:19:38 +0900

    Finally getting somewhere

--- Textbook_Track/assistant.py
+++ Textbook_Track/assistant.py
@@ -1,153 +1,18 @@
-#This is code, taken from another coding project, is modified and used to create the library of all possible, single, double and triple replacement mutants for each kmer in the problem solution ba1i
+#tick tock analogue
 
-#pattern constraints k <= 12, d <= 3
+#tictoc functions for debugging
+def tic():
+    #Homemade version of matlab tic and toc functions
+    import time
+    global startTime_for_tictoc
+    startTime_for_tictoc = time.time()
 
-import pandas as pd
-import regex as re
+def toc():
+    import time
+    if 'startTime_for_tictoc' in globals():
+        print("Elapsed time is " + str(time.time() - startTime_for_tictoc) + " seconds.")
+    else:
+        print("Toc: start time not set")
 
-#make a library of single mutants, i.e. d=1
-def single_mutants(wt, key):
-
- #Read in the sequence and send to a dataframe
- table = []
- table.append(list(wt))
- df = pd.DataFrame(table)
- mut_colno = len(wt) 
-
- #prepare a list of available bases
- bases = ['A', 'C', 'G', 'T']
- altbase = []
-
- #set the mutant seq number counter
- seq = 0
- counter = 0
-
- #iterate through the base list, until we hit a match with bases of the wildtype sequence (df columns)
- for base in bases:
-  for pos in df.columns:
-
-   #create regext of current position, to search mutation info, preventing back-mutations)
-   pattern = r'{}'.format(pos+1)
-   matchObj = re.search(pattern, key)
-
-   #so if the base matches the wildtype and the position of previous mutation has not been covered
-   if base == df.ix[0,pos] and matchObj == None:
-    
-    #prepare a list of alternate bases and use those to start new corresponding mutant seqs
-    altbase.extend(bases)
-    altbase.remove(base)
-    df.set_value(seq+1, pos, altbase.pop())
-    df.set_value(seq+2, pos, altbase.pop())
-    df.set_value(seq+3, pos, altbase.pop())
-
-    #summarise mutation info to new column, provided the mutants have not been seen before
-    df.set_value(seq+1, mut_colno, '{}{}{}, {}'.format(base,pos+1,df.ix[seq+1,pos],key))
-    df.set_value(seq+2, mut_colno, '{}{}{}, {}'.format(base,pos+1,df.ix[seq+2,pos],key))
-    df.set_value(seq+3, mut_colno, '{}{}{}, {}'.format(base,pos+1,df.ix[seq+3,pos],key))
-    seq += 3
- 
- #replace the rest of the dataframe (empty mutant seq bases) with wildtype bases
- df = df.fillna(df.ix[0,df.columns])
- 
- 
- #change the column name for the mutations
- df = df.rename(columns = {mut_colno: 'Mutation'})
-
- #drop the wild type and rows where no mutation info was set, i.e where the same mutation has already happened
- df = df[df.Mutation.notnull()]
- 
- return df.reset_index(drop = True)
-
-
-def library_maker(df):
- #call the single mutants function and transpose the df, in order to count right (i.e., along the index)
- dft = df.T
- shape = len(df.columns) #to get column index, which is the mutation info, which will be used to create the keys
- mutant_dictionary = {}
- 
- #put sequences in the library for the double replacement mutant library
- for mu in dft:
-  mutant_dictionary.update({df.ix[mu,(shape-1)] : df.ix[mu,0:(shape-1)].str.cat()})
- 
- return mutant_dictionary
-
-
-def double_mutants(library):
- 
-#essentially what we're doing here is looping through the single mutant library, calling the single_mutant function again on each single_mutant to make the double mutants (this will naturally create duplicates). serially append each sub df to make double mutant df, then drop duplicates.
- ddf = pd.DataFrame()
- shape = len(ddf.columns)
-
- for key, seq in library.items():
-  ddf = ddf.append(single_mutants(seq, key), ignore_index = True)
- 
- return ddf.drop_duplicates(ddf.columns[0:(shape-1)], keep = 'last').reset_index(drop = True)
-
-def triple_mutants(library):
- 
-#essentially what we're doing here is looping through the double mutant library, calling the single_mutant function again on each double_mutant to make the triple mutants (this will naturally create duplicates). serially append each sub df to make triple mutant df, then drop duplicates.
- ddf = pd.DataFrame()
- shape = len(ddf.columns)
-
- for key, seq in library.items():
-  ddf = ddf.append(single_mutants(seq, key), ignore_index = True)
- 
- return ddf.drop_duplicates(ddf.columns[0:(shape-1)], keep = 'last').reset_index(drop = True)
-
-
-#This writes the various mutant libraries to strings and and puts them in lists
-def d_cons(wt, d):
- 
- kmers = []
-
- if d == 3:
-  
-  df = single_mutants(wt, '')
-  dft = df.T
-  shape = len(df.columns)
-  for i in dft:
-   kmers.append(df.ix[i,0:(shape-1)].str.cat().upper())
-  
-  lib2 = library_maker(df)
-
-  df = double_mutants(lib2)
-  dft = df.T
-  shape = len(df.columns)
-  for i in dft:
-   kmers.append(df.ix[i,0:(shape-1)].str.cat().upper())
-
-  lib3 = library_maker(df)
-
-  df = triple_mutants(lib3)
-  dft = df.T
-  shape = len(df.columns)
-  for i in dft:
-   kmers.append(df.ix[i,0:(shape-1)].str.cat().upper())
-
- elif d == 2:
-  
-  df = single_mutants(wt, '')
-  dft = df.T
-  shape = len(df.columns)
-  for i in dft:
-   kmers.append(df.ix[i,0:(shape-1)].str.cat().upper())
-  
-  lib2 = library_maker(df)
-
-  df = double_mutants(lib2)
-  dft = df.T
-  shape = len(df.columns)
-  for i in dft:
-   kmers.append(df.ix[i,0:(shape-1)].str.cat().upper())
-
- elif d == 1:
- 
-  df = single_mutants(wt, '')
-  dft = df.T
-  shape = len(df.columns)
-  for i in dft:
-   kmers.append(df.ix[i,0:(shape-1)].str.cat().upper())
-  
- return kmers
 
                 
--- Textbook_Track/messy_freqword_ba1i.py
+++ Textbook_Track/messy_freqword_ba1i.py
@@ -6,10 +6,13 @@
 # 3) use the hamming distance calculator determine how often each of these variants score a hit in the original sequence, everytime there is a hit add the variant to the 'count' list
 # 4) find the variants that represent the modes of count and output to command line
 
-#function to split the DNA into k-mers, 
+from assistant import *
+
+#function to split the DNA into k-mers
+
 def dnaread(dna, k):
  i = 0
- kmers = []
+ kmers = [] 
  while i <= (len(dna) - k):
   kmers.append(dna[i:i+k])
   i += 1
@@ -20,23 +23,38 @@ def dnaread(dna, k):
 def HammingDistance(p,q):
  return(sum(b1!=b2 for b1,b2 in zip(p,q)))
 
+from itertools import combinations,product
+
+#function to get all mutants(mismatch variants), from stack: http://stackoverflow.com/a/19890945/7800823
+def mismatch(word, i = 2):
+ for d in range(i+1):
+  for locs in combinations(range(len(word)), d):
+   thisWord = [[char] for char in word]
+   for loc in locs:
+    origChar = word[loc]
+    thisWord[loc] = [l for l in "ACGT" if l != origChar]
+   for poss in product(*thisWord):
+    yield "".join(poss)    
+
 #function to find and store k-mer positions in a list, mod from ba1d
 def messy_posfind(dna, pattern, k, d):
  i = 0
- posse = []
+ hits = 0
  while i <= (len(dna) - k):
   if (HammingDistance(dna[i:i+k], pattern)) <= d:
-   posse.append(pattern)
+   hits += 1
    i += 1
   else:
    i += 1
- #print(posse)
- return posse
+
+ return (hits, pattern)
 
 #use a fxn to find multiple modes, this step seems to take the most time, I hate using lamda fxns but stack overflow don't think there is any faster way to write a 'mode' function
+
 def mode(array):
     most = max(list(map(array.count, array)))
     return list(set(filter(lambda x: array.count(x) == most, array)))
+
 
 #Get the input data
 with open('rosalind_ba1i.txt', 'r') as inputfile:
@@ -44,37 +62,38 @@ with open('rosalind_ba1i.txt', 'r') as inputfile:
  inps = inputfile.readline().strip().split(" ")
  k, d = int(inps[0]), int(inps[1])
 
+
 #first get list of all the occuring kmers, of k size
 kmers = dnaread(dna, k)
-#print(kmers)
-#get functions from other file to list the mutants of all kmers
-from assistant import *
 
 #open list of matched mismatches
-
 mutant_kmers = []
 all_kmers = []
+
 #for each of the possible kmers:
+#######
+tic()
+#Most amount of time
 for kmer in kmers:
- all_kmers.append(kmer)
  #create all possible variants, with number of mutations representing d
- mutant_kmers = d_cons(kmer, d)
+ mutant_kmers = list(mismatch(kmer, d))
  #this step just so I don't get lists within list
  for e in mutant_kmers:
   all_kmers.append(e)
+toc()
+#########
+
 temp = []
 counts = []
-final = set(all_kmers)
 
+final = set(all_kmers)
+tic()
+from heapq import heappushpop, heappop, nlargest, heappush
 for fin in final:
- temp = messy_posfind(dna, fin, k, d)
- #this step just so I don't get lists within list
- for el in temp:
-  counts.append(el)
-#print(counts)
-max_kmers = mode(counts)
-#print(max_kmers)
-for word in max_kmers:
- print(word, end=' ')
- 
+ heappush(counts, (messy_posfind(dna, fin, k, d)))
+toc()
+
+
+print(nlargest(4, counts, key=lambda x: x[0]))
+
 
